1cdfc5e775d04a17974f3aaad8fd99e253ceb1bb
Alberto Vazquez <albertovmrm@gmail.com>
2021-03-21 20:01:10 +0100

    Convert rank process to binary number

--- P1/src/hipercubo.c
+++ P1/src/hipercubo.c
@@ -7,11 +7,15 @@
 #include <definitions.h>
 #include "/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h"
 
+void assign_neighbours(int rank, int size, int neighbours[]);
+int* fix_binary_number(int binary[], int truncated, int aux);
+int* convert_decimal_to_binary(int number, int truncated);
+
 /* Main function */
 int main(int argc, char **argv) {
-    int rank, size, trucanted, hypercube = false;
+    int rank, size, truncated, hypercube = false;
     float buf;
-
+    
 	MPI_Request request;
     MPI_Status status;
 
@@ -21,9 +25,9 @@ int main(int argc, char **argv) {
     
     if (rank == 0) {
         double res = log(size)/log(2);
-        trucanted = (int) res;
+        truncated = (int) res;
 
-        if (trucanted == res) {
+        if (truncated == res) {
             hypercube = true;
             send_network_topology_confirmation(hypercube, size, request);
 
@@ -39,9 +43,59 @@ int main(int argc, char **argv) {
     }
 
     if (hypercube == true) {
+        int neighbours[truncated];
+        
         MPI_Recv(&buf, 1, MPI_FLOAT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        printf("Process %d received %.2f\n", rank, buf);
+        //printf("Process %d received %.2f\n", rank, buf);
+
+        assign_neighbours(rank, log(size)/log(2), neighbours);
     }
 
     MPI_Finalize();
+}
+
+/* Fix numbers which length is less than the dimension of the network */
+int* fix_binary_number(int binary[], int truncated, int aux) {
+    int *b_digits;
+    int i;
+    int real_binary;
+
+    for (i = 0; i < aux; i++) {
+        b_digits[i] = 0;
+    }
+
+    for (int j = truncated-aux-1; 0 <= j; j--) {
+        b_digits[i] = binary[j];
+        i++;
+    }
+
+    return b_digits;
+}
+
+/* Convert a given decimal number to binary number */
+int* convert_decimal_to_binary(int number, int truncated) {
+    int binary[truncated];
+    int i;
+    int aux = truncated;
+    int real_binary[truncated];
+
+    for (i = 0; number > 0; i++) {
+        binary[i] = number%2;
+        number = number/2;
+        aux--;
+    }
+
+    return fix_binary_number(binary, truncated, aux);
+}
+
+/* Assign neighours to processes */
+void assign_neighbours(int rank, int truncated, int neighbours[]) {
+
+    int* binary = convert_decimal_to_binary(rank, truncated);
+    
+    printf("Number %d --> ", rank);
+    for (int i = 0; i < truncated; i++) {
+        printf("%d",binary[i]);
+    }
+    printf("\n");
 }
