c3d76c98da26d891484bbc201508014011551a49
Alberto Vazquez <albertovmrm@gmail.com>
2021-03-30 13:13:56 +0200

    First approach to set view

--- P2/src/pract2.c
+++ P2/src/pract2.c
@@ -1,15 +1,16 @@
 /* Pract2  RAP 09/10    Javier Ayllon*/
-
 #include <openmpi/mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <X11/Xlib.h> 
-#include <assert.h>   
-#include <unistd.h>   
-#define NIL (0)       
-#define NUM_WORKERS_PROCESS 5
-/*Variables Globales */
+#include <X11/Xlib.h>
+#include <assert.h>
+#include <unistd.h>
 
+#define NIL (0)
+#define NUM_WORKERS_PROCESS 5
+#define BUFSIZE 100
+
+/*Variables Globales */
 XColor colorX;
 Colormap mapacolor;
 char cadenaColor[]="#000000";
@@ -57,7 +58,6 @@ void dibujaPunto(int x,int y, int r, int g, int b) {
 }
 
 /* Programa principal */
-
 int main (int argc, char *argv[]) {
 
       int rank, size, tag;
@@ -78,10 +78,20 @@ int main (int argc, char *argv[]) {
             printf("Soy el padre %d\n", rank);
 	      /* Codigo del maestro */
 
-            /*for (int i = 0; i < 400; i++) {
-                  MPI_Recv();
-                  dibujaPunto(buf[0], buf[1], buf[2], buf[3], buf[4]);
+            /*for (int x = 0; i < 400; x++) {
+                  for (int y = 0; i < 400; y++) {
+                        MPI_Recv();
+                        MPI_Recv(&toroidal, 1, MPI_INT, 0, MPI_ANY_TAG, intercomm, &status);
+                        dibujaPunto(x, y, buf[0], buf[1], buf[2]);
+                  }
             }*/
+            
+            /*
+            for (int = 0; i < 1600; i++) {
+                  MPI_Recv(&buf, 5, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, intercomm, &status);
+                  dibujaPunto(buf[0], buf[1], buf[2], buf[3], buf[4]);
+            }
+            */
 
 	      /*En algun momento dibujamos puntos en la ventana algo como
 	      dibujaPunto(x,y,r,g,b);  */
@@ -91,23 +101,46 @@ int main (int argc, char *argv[]) {
       }
 
       else {
-            MPI_Info info;
             MPI_File fh;
-            MPI_Datatype arraytype;
-            MPI_Offset disp;
+            MPI_Offset filesize;
             MPI_Status status;
-            
+            int bufsize, *buffer, k = 0;
+
             printf("Hijo %d\n", rank);
-            //MPI_Comm_get_parent(&intercomm);
+
+            //lines = size_child_comm/400;
+
             /* Codigo de todos los trabajadores */
             /* El archivo sobre el que debemos trabajar es foto.dat */
 
             MPI_File_open(MPI_COMM_WORLD, "data/foto.dat", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-            MPI_File_set_view(fh, disp, MPI_UNSIGNED_CHAR, arraytype, "native", MPI_INFO_NULL);
-            MPI_File_read(fh, buf, 5, MPI_UNSIGNED_CHAR, &status);
+            MPI_File_get_size(fh, &filesize);
+            filesize = filesize / sizeof(char);
+            bufsize = filesize / NUM_WORKERS_PROCESS;
+            printf("%d\n",bufsize);
+            buffer = (int *) malloc(bufsize * sizeof(char));
+            
+            MPI_File_set_view(fh, rank * bufsize * sizeof(char), MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, "native", MPI_INFO_NULL);
+            MPI_File_read(fh, buffer, bufsize, MPI_UNSIGNED_CHAR, &status);
+            MPI_File_close(&fh);
 
-            printf("%s\n",buf);
-            //MPI_Send();
+            //printf("%d %d %d %d %d\n",buffer[0],buffer[1],buffer[2], buf[3], buf[4]);
+
+            /*
+            for (int x = 0; x < 400; x++) {
+                  for (int y = 0; y < 400/NUM_WORKERS_PROCESS; y++) {
+                        buf[0] = x;
+                        buf[1] = y;
+
+                        for (int i = 0; i < 3; i++) {
+                              buf[cnt++] = buffer[k++];
+                        }
+                        cnt = 0;
+                        
+                        MPI_Send(buf,);
+                  }
+            }
+            */
       }
 
       MPI_Finalize();
